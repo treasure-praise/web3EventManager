67dd245c261a298ad6e442f6c77e564777920de1
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-08-12 17:18:46 +0100

    defined smart contract and info to store on-chain

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -1,2 +1,18 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+contract Web3RSVP {
+
+    //struct is similar to a JS object in that it stores related information about an entity.
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+}    
